371ead61d6f75d57bb5cbb1f1fedccf00c847b52
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-06-12 00:08:50 -0500

    feat: update chat screen

--- src/app/chat.tsx
+++ src/app/chat.tsx
@@ -1,7 +1,7 @@
 import { generateAPIUrl } from '@/utils/utils';
 import { useChat } from '@ai-sdk/react';
 import { fetch as expoFetch } from 'expo/fetch';
-import { View, TextInput, ScrollView, Text, ViewStyle } from 'react-native';
+import { View, TextInput, ScrollView, Text, ViewStyle, TextStyle } from 'react-native';
 import { Screen } from '@/components';
 import { useAppTheme } from '@/utils/useAppTheme';
 import { spacing, ThemedStyle } from '@/theme';
@@ -15,18 +15,32 @@ export default function ChatScreen() {
 
   const { theme, themed } = useAppTheme()
 
-  if (error) return (
-    <Screen safeAreaEdges={["top"]} contentContainerStyle={themed($container)}>
+
+  return (
+    <Screen safeAreaEdges={["top", "bottom"]} contentContainerStyle={themed($container)}>
       <View
         style={themed($topContainer)}
       >
-        <Text>{error.message}</Text>
+        <ScrollView style={{ flex: 1 }}>
+          {messages.map(m => (
+            <View key={m.id} style={{ marginVertical: 8 }}>
+              <View>
+                <Text style={{ fontWeight: 700 }}>{m.role}</Text>
+                <Text>{m.content}</Text>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+
       </View>
-      <View
-        style={themed($bottomContainer)}
-      >
+      <View style={themed($bottomContainer)}>
+        {error && (
+          <View style={themed($errorContainer)}>
+            <Text style={themed($errorText)}>{error.message}</Text>
+          </View>
+        )}
         <TextInput
-          style={{ backgroundColor: 'white', padding: spacing.sm}}
+          style={themed($input)}
           placeholder="Say something..."
           value={input}
           onChange={e =>
@@ -46,48 +60,6 @@ export default function ChatScreen() {
         />
       </View>
     </Screen>
-  )
-
-
-  return (
-    <Screen safeAreaEdges={["top"]} contentContainerStyle={themed($container)}>
-      <View
-        style={themed($topContainer)}
-      >
-        <ScrollView style={{ flex: 1 }}>
-          {messages.map(m => (
-            <View key={m.id} style={{ marginVertical: 8 }}>
-              <View>
-                <Text style={{ fontWeight: 700 }}>{m.role}</Text>
-                <Text>{m.content}</Text>
-              </View>
-            </View>
-          ))}
-        </ScrollView>
-
-        <View style={themed($bottomContainer)}>
-          <TextInput
-            style={{ backgroundColor: 'white', margin: spacing.md, padding: spacing.md }}
-            placeholder="Say something..."
-            value={input}
-            onChange={e =>
-              handleInputChange({
-                ...e,
-                target: {
-                  ...e.target,
-                  value: e.nativeEvent.text,
-                },
-              } as unknown as React.ChangeEvent<HTMLInputElement>)
-            }
-            onSubmitEditing={e => {
-              handleSubmit(e);
-              e.preventDefault();
-            }}
-            autoFocus={true}
-          />
-        </View>
-      </View>
-    </Screen>
   );
 }
 
@@ -100,10 +72,31 @@ const $topContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
   flex: 9,
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'center',
   paddingHorizontal: spacing.lg,
 })
 
 const $bottomContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
   flex: 1,
+})
+
+const $errorContainer: ThemedStyle<ViewStyle> = ({ colors, spacing }) => ({
+  position: 'absolute',
+  bottom: '100%',
+  left: spacing.md,
+  right: spacing.md,
+  backgroundColor: colors.error,
+  padding: spacing.xs,
+  borderRadius: spacing.xs,
+  marginBottom: spacing.xs
+})
+
+const $errorText: ThemedStyle<TextStyle> = ({ colors }) => ({
+  color: colors.background,
+  textAlign: 'center'
+})
+
+const $input: ThemedStyle<TextStyle> = ({ spacing }) => ({
+  backgroundColor: 'white',
+  margin: spacing.md,
+  padding: spacing.md
 })
