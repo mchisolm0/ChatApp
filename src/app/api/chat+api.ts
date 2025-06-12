426c653925eab35ab51b9e44c6c8c41db6e7709b
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-06-11 23:14:04 -0500

    feat: create api route

---
+++ src/app/api/chat+api.ts
@@ -0,0 +1,18 @@
+import { google } from '@ai-sdk/google';
+import { streamText } from 'ai';
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: google('gemini-2.0-flash'),
+    messages,
+  });
+
+  return result.toDataStreamResponse({
+    headers: {
+      'Content-Type': 'application/octet-stream',
+      'Content-Encoding': 'none',
+    },
+  });
+}
