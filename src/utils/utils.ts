9e9957e684e009d84917ac88ff9cb17c480c0e0b
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-06-11 23:49:32 -0500

    feat: update API URL generation

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,13 +1,14 @@
 import Constants from 'expo-constants';
 
 export const generateAPIUrl = (relativePath: string) => {
-  console.log("Constants.experienceUrl" + Constants.experienceUrl);
-  const origin = Constants.experienceUrl.replace('exp://', 'http://');
-
   const path = relativePath.startsWith('/') ? relativePath : `/${relativePath}`;
 
   if (process.env.NODE_ENV === 'development') {
-    return origin;
+    // For development builds, use localhost if experienceUrl is undefined
+    if (!Constants.experienceUrl) {
+      return `http://localhost:8081${path}`;
+    }
+    return Constants.experienceUrl.replace('exp://', 'http://');
   }
 
   if (!process.env.EXPO_PUBLIC_API_BASE_URL) {
