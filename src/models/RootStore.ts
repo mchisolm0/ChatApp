9b03915cad98d9be53363a5f3e498c123b36e2cc
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-06-18 07:47:37 -0500

    fix: properly initialize store models with create() instead of empty objects

--- src/models/RootStore.ts
+++ src/models/RootStore.ts
@@ -7,15 +7,15 @@ import { ChatStoreModel } from "./ChatStore"
 import { AuthStoreModel } from "./AuthStore"
 
 export const RootStoreModel = types.model("RootStore").props({
-  chatStore: types.optional(ChatStoreModel, {} as any),
-  authStore: types.optional(AuthStoreModel, {} as any),
+  chatStore: types.optional(ChatStoreModel, () => ChatStoreModel.create({})),
+  authStore: types.optional(AuthStoreModel, () => AuthStoreModel.create({})),
 })
 
 /**
  * The RootStore instance.
  */
-export interface RootStore extends Instance<typeof RootStoreModel> {}
+export interface RootStore extends Instance<typeof RootStoreModel> { }
 /**
  * The data of a RootStore.
  */
-export interface RootStoreSnapshot extends SnapshotOut<typeof RootStoreModel> {}
+export interface RootStoreSnapshot extends SnapshotOut<typeof RootStoreModel> { }
