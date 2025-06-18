f3ee78b0479ad5852913fbab85235c032b8a24ba
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-06-18 08:19:37 -0500

    refactor: replace Stack with Slot component in root layouts

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,14 +1,12 @@
 import React from "react"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
-import { Stack } from "expo-router"
+import { Slot } from "expo-router"
 
 export default function RootLayout() {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack>
-        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-      </Stack>
+      <Slot />
     </GestureHandlerRootView>
   )
 }
--- src/components/RootLayout.web.tsx
+++ src/components/RootLayout.web.tsx
@@ -1,13 +1,11 @@
 import { View } from "react-native"
-import { Stack } from "expo-router"
+import { Slot } from "expo-router"
 
 export default function RootLayout() {
 
   return (
     <View style={{ flex: 1 }}>
-      <Stack>
-        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-      </Stack>
+      <Slot />
     </View>
   )
 }
