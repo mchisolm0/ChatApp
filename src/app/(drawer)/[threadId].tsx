0a660ba35152ec800aba476792d9ff455b3a7bea
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-06-24 09:16:22 -0500

    chore: remove unused imports

--- src/app/(drawer)/[threadId].tsx
+++ src/app/(drawer)/[threadId].tsx
@@ -1,6 +1,4 @@
-import { useLocalSearchParams } from 'expo-router';
 import { ChatInterface } from '@/components/ChatInterface';
-import { Id } from 'convex/_generated/dataModel';
 
 export default function ChatScreen() {
   return <ChatInterface />;
